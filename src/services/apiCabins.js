504929920ecc16a274e80374fc4e63dbb56e9ff3
Hendra Putu <mchen.putu@gmail.com>
2024-08-06 15:35:00 +0800

    query mutate (delete)

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -10,3 +10,14 @@ export async function getCabins() {
 
   return data;
 }
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be deleted");
+  }
+
+  return data;
+}
